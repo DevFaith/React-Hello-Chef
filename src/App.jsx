5b802fb572ab6b64d85440c286a0c7ddbda50303
DevFaith <waitherafaith@outlook.com>
2024-04-13 02:45:20 +0300

    components with a lower case c

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './Components/Header'
-import Hero from './Components/Hero'
+import Header from './components/Header'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
