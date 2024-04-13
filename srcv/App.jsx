b3f035ce5ad5bd81714aa8f4cff5d2435aeaa6d4
developer-assets <999patrickobama@gmail.com>
2024-04-13 03:14:49 +0300

    Fixing

---
+++ srcv/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <div className='font-outfit'>
+      <Header />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
